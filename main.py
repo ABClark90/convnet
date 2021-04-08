f0effd31657f62eff3d462dcbac613059ce3ba69
ABClark90 <55220265+ABClark90@users.noreply.github.com>
2021-04-09 06:58:31 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#ignore those messages
+
+import tensorflow as tf
+import sys
+from tensorflow import keras
+from tensorflow.keras import layers, regularizers
+from tensorflow.keras.datasets import cifar10
+#cifar10 is natural images, animals trucks etc, 50k training 10k test, 32x32 rgb
+
+(x_train, y_train), (x_test, y_test) =cifar10.load_data()
+x_train = x_train.astype('float32')/255
+#/255 to normalise, get values between 0 and 1, similar to making greyscale
+x_test = x_test.astype('float32')/255
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(32, 3, padding='valid', activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10)
+        #want output to be 32 w/ kernel size to 3
+    ]
+)
+print(model.summary())
+
+def my_model():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3, padding='same', kernel_regularizer=keras.regularizers.l2(0.01),)(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 5, padding='same', kernal_regularizer=keras.regularizers.l2(0.01),)(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Conv2D(128, 3, padding='same', kernal_regularizer=keras.regularizers.l2(0.01),)(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation='relu', kernel_regularizer=keras.regularizers.l2(0.01,))(x)
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+model = my_model()
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(lr=3e-4),
+    metrics=["accuracy"]
+)
+model.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+model.evaluate(x_test, y_test, batch_size=64, verbose=2)
